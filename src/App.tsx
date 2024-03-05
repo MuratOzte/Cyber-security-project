a12640306f107910322bdbe9f5cf836d1e5a810e
MuratOzte <mo2553481@gmail.com>
2024-03-06 01:53:21 +0300

    Add navigation component to App.js and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import Nav from './components/nav/nav';
 function App() {
-    return <></>;
+    return (
+        <div className="w-full h-16">
+            <Nav />
+        </div>
+    );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ['./src/**/*.{html,tsx,js,ts}'],
     theme: {
-      extend: {},
+        extend: {},
     },
     plugins: [],
-  }
+};
