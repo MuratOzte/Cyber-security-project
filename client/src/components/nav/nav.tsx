ef7f242560c0754d0bfb7cd1da035d2163fc25f9
MuratOzte <mo2553481@gmail.com>
2024-03-10 17:47:00 +0300

    Update styles and placeholders in hero and nav components

--- client/src/components/nav/nav.tsx
+++ client/src/components/nav/nav.tsx
@@ -5,10 +5,11 @@ const Nav = () => {
         <>
             <nav className="bg-nav w-full h-full">
                 <div className="container mx-auto flex justify-between items-center h-full px-12 py-2">
-                    <div>
+                    <div className="flex items-center">
                         <a href="/" className="text-white text-2xl font-bold">
-                            <img src={logo} alt="logo" className='w-16' />
+                            <img src={logo} alt="logo" className="w-16" />
                         </a>
+                        <p className="text-[24px]">Easyber Security</p>
                     </div>
                     <div>
                         <ul className="flex space-x-4">
