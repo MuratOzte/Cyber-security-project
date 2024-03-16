6fd4d402c0ed71a8fee9f6147e0f70d6b8c8878d
MuratOzte <mo2553481@gmail.com>
2024-03-16 15:36:28 +0300

    Refactor layout and styling in App and Hero components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,19 @@ import Hero from './components/home/hero';
 import Nav from './components/nav/nav';
 function App() {
     return (
-        <div className="snap-y">
-            <Hero />
-            <Attacks />
-        </div>
+        <>
+            <div className="snap-mandatory snap-y h-screen overflow-y-scroll">
+                <div className="snap-center snap-always">
+                    <Hero />
+                </div>
+                <div className="snap-center snap-always">
+                    <Attacks />
+                </div>
+                <div className="snap-center snap-always">
+                    <Attacks />
+                </div>
+            </div>
+        </>
     );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import Nav from './components/nav/nav.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import Nav from './components/nav/nav.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Nav />
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Nav />
+        <App />
+    </React.StrictMode>
+);
