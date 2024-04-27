8cd822c8c47a6054c7036afb89b1813c71360f4c
MuratOzte <mo2553481@gmail.com>
2024-04-27 21:40:52 +0300

    Refactor Attack component to include Select component and update styles

--- client/src/components/Attack/Select.tsx
+++ client/src/components/Attack/Select.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const Attacks = [
     {
         name: 'Attack 1',
@@ -14,16 +16,37 @@ const Attacks = [
 ];
 
 const Select = () => {
+    const [selectedAttacks, setSelectedAttacks] = useState([]);
+
     return (
         <div className="flex justify-center mt-5 w-2/3">
             {Attacks.map((attack) => {
                 return (
                     <div
                         key={attack.name}
-                        className="flex items-center flex-col text-center w-2/12 m-auto shadow-lg pb-2 rounded-lg bg-gray-100 hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 text-gray-700 text-lg ring-[4px] ring-green-600 uppercase font-sans"
+                        onClick={() => {
+                            setSelectedAttacks((prev) => {
+                                if (prev.includes(attack.name)) {
+                                    return prev.filter(
+                                        (item) => item !== attack.name
+                                    );
+                                } else {
+                                    return [...prev, attack.name];
+                                }
+                            });
+                        }}
+                        className={`flex items-center flex-col text-center w-2/12 m-auto rounded-lg  bg-gray-100 hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 text-gray-700 text-lg uppercase font-sans shadow-green-500 shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset] ${
+                            !selectedAttacks.includes(attack.name)
+                                ? 'shadow-none'
+                                : null
+                        }`}
                     >
-                        <img src={attack.img} alt={attack.name} />
-                        <h1>{attack.name}</h1>
+                        <img
+                            src={attack.img}
+                            alt={attack.name}
+                            className="rounded-lg rounded-bl-none rounded-br-none"
+                        />
+                        <h1 className="py-1 select-none">{attack.name}</h1>
                     </div>
                 );
             })}
